2ba26467059b9133301f0ff0a62c2ec01487ecad
duyhoang5891 <hoang.phamduy5891@gmail.com>
2024-07-28 19:42:00 +0700

    update evident

--- udagram-frontend/src/environments/environment.prod.ts
+++ udagram-frontend/src/environments/environment.prod.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'http://a9ed4a69977094139af8ecbae301e69e-1149356182.us-west-2.elb.amazonaws.com:8080/api/v0'
 };
 
 /*
