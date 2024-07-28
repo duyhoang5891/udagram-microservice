8cbb6ec801370801ba83ab05142e302c2e7b6eee
duyhoang5891 <hoang.phamduy5891@gmail.com>
2024-07-28 18:43:05 +0700

    update proxy

--- udagram-frontend/src/environments/environment.prod.ts
+++ udagram-frontend/src/environments/environment.prod.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://a2fac0cd513f94e83a3433b918279075-1698170132.us-west-2.elb.amazonaws.com:8080/api/v0'
+  apiHost: 'http://localhost:8080/api/v0'
 };
 
 /*
